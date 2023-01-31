3a02ee349b8e737ac5c70f43e19b914cda77f5a9
Mr-Bakai <ismailov.time@gmail.com>
2023-01-31 17:09:55 +0600

    Initial Commit

---
+++ TransfersUI/TransfersUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransfersUIApp.swift
+//  TransfersUI
+//
+//  Created by Bakai on 31/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TransfersUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
